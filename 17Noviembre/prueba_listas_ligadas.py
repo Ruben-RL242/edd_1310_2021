57373ffce2cdcb37dd09db06574d6ac0470e70b9
Ruben-RL242 <cristopherrubenroblesleon@gmail.com>
2020-11-19 12:54:40 -0600

    Se agrega la clase del 19noviembre de lista ligadas con metodos

--- 17Noviembre/prueba_listas_ligadas.py
+++
@@ -1,9 +0,0 @@
-from listas import LinkedList
-l = LinkedList()
-print(f'L está vacía? { l.is_empty()}')
-l.append(10)
-l.append(5)
-l.append(6)
-l.append(20)
-print(f'L está vacía? { l.is_empty()}')
-l.transversal()
