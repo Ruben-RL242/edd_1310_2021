7a16e7fe31b285f79f503d8a43116add146f61d7
Ruben-RL242 <cristopherrubenroblesleon@gmail.com>
2021-01-11 19:24:50 -0600

    Tarea 10 Barco

--- adts/Tarea10/pruebas_colas.py
+++ adts/Tarea10/pruebas_colas.py
@@ -1,25 +1,5 @@
 from colas import Queue,BoundedPriorityQueue
 
-q1 = Queue()
-q1.enqueue(3)
-q1.enqueue(33)
-q1.enqueue(23)
-print(q1.to_string())
-
-print("Prueba 2 de Queue")
-c1 = {"id":1 , "nombre": "Mario" , "balance": 20.5}
-c2 = {"id":2 , "nombre": "Diana" , "balance": 3456.5}
-c3 = {"id":3 , "nombre": "Mario" , "balance": 1000000.0}
-
-atencion = Queue()
-atencion.enqueue(c1)
-atencion.enqueue(c2)
-atencion.enqueue(c3)
-print(atencion.to_string())
-sig= atencion.dequeue()
-print(f"Bienvenido sr. {sig['nombre']}, en que podemos servirle el dia de hoy?")
-print(atencion.to_string())
-
 
 print("=================Pruebas de las colas con prioridad acotada======================")
 maestre = {"prioridad":4 , "descripción":"Maestre","persona":"juan P"}
